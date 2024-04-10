bd06f9b02aee5a41574e0d5a904676b2dd4c3930
ksmles36 <ksmles36@gmail.com>
2024-04-11 01:32:15 +0900

    Initial commit

---
+++ src/main/java/com/ksm/querydslstudy/Hello.java
@@ -0,0 +1,2 @@
+package com.ksm.querydslstudy;public class Hello {
+}
---
+++ src/test/java/com/ksm/querydslstudy/QueryDslStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksm.querydslstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
