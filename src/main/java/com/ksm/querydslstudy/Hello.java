eea39b1bd0ae195ceca4db933d58f704f663bf35
ksmles36 <ksmles36@gmail.com>
2024-04-11 01:41:13 +0900

    세팅 진행중 2

--- src/main/java/com/ksm/querydslstudy/Hello.java
+++ src/main/java/com/ksm/querydslstudy/Hello.java
@@ -1,4 +1,16 @@
 package com.ksm.querydslstudy;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Hello {
+    @Id
+    @GeneratedValue
+    private Long id;
 }
