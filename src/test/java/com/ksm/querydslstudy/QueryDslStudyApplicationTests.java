381c1ae1955d6a117e04d6db80299cd034f4bb49
ksmles36 <ksmles36@gmail.com>
2024-04-11 07:49:30 +0900

    쿼리 dsl 세팅 후 테스트코드로 세팅 완료됐는 지 검증까지

--- src/test/java/com/ksm/querydslstudy/QueryDslStudyApplicationTests.java
+++ src/test/java/com/ksm/querydslstudy/QueryDslStudyApplicationTests.java
@@ -1,13 +1,34 @@
 package com.ksm.querydslstudy;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Transactional
 class QueryDslStudyApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = jpaQueryFactory
+                .selectFrom(qHello)
+                .fetchOne();
+
+        Assertions.assertEquals(hello, result);
+        Assertions.assertEquals(hello.getId(), result.getId());
     }
 
 }
