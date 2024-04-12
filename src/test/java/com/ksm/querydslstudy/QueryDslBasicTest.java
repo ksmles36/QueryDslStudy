b31011e6cacb5e1b9876794932d3e8f405a6c83b
Kim Seongmin <45448156+ksmles36@users.noreply.github.com>
2024-04-12 19:47:00 +0900

    정렬 까지 실습

--- src/test/java/com/ksm/querydslstudy/QueryDslBasicTest.java
+++ src/test/java/com/ksm/querydslstudy/QueryDslBasicTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static com.ksm.querydslstudy.entity.QMember.member;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -74,6 +76,73 @@ public class QueryDslBasicTest {
 
         assertEquals(fetchOne.getUsername(), "member1");
         assertEquals(fetchOne.getAge(), 10);
+    }
+
+    @Test
+    public void searchAndParam() {
+        Member fetchOne = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"), member.age.eq(10))
+                .fetchOne();
+
+        assertEquals(fetchOne.getUsername(), "member1");
+        assertEquals(fetchOne.getAge(), 10);
+    }
+
+    @Test
+    public void resultFetch() {
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        Member member1 = queryFactory
+                .selectFrom(member)
+                .orderBy(member.id.asc())
+                .fetchFirst();
+
+        //부트 3.x 쿼리dsl 5.0 이상 부터는 페이징정보 포함 메소드가 비권장으로 바뀜.
+//        List<Member> fetch1 = queryFactory
+//                .selectFrom(member)
+//                .offset(pageable.getOffset())
+//                .limit(pageable.getPageSize())
+//                .fetch();
+
+        Long l = queryFactory
+                .select(member.count())
+                .from(member)
+                .fetchOne();
+
+        System.out.println("fetch = " + fetch);
+        System.out.println("member1 = " + member1);
+//        System.out.println("fetch1 = " + fetch1);
+        System.out.println("l = " + l);
+    }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순(desc)
+     * 2. 회원 이름 올림차순(asc)
+     * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertEquals(member5.getUsername(), "member5");
+        assertEquals(member6.getUsername(), "member6");
+        assertEquals(memberNull.getUsername(), null);
     }
 
 
