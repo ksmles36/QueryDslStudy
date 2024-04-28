eddc6408594578ad6433698ae64e5c8b7069966a
ksmles36 <ksmles36@gmail.com>
2024-04-28 15:44:15 +0900

    기본 프로젝션 실습 완료

--- src/test/java/com/ksm/querydslstudy/QueryDslMiddleTest.java
+++ src/test/java/com/ksm/querydslstudy/QueryDslMiddleTest.java
@@ -1,9 +1,13 @@
 package com.ksm.querydslstudy;
 
 import com.ksm.querydslstudy.dto.MemberDto;
+import com.ksm.querydslstudy.dto.UserDto;
 import com.ksm.querydslstudy.entity.Member;
+import com.ksm.querydslstudy.entity.QMember;
 import com.ksm.querydslstudy.entity.Team;
+import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
@@ -79,6 +83,81 @@ public class QueryDslMiddleTest {
 
         for (MemberDto memberDto : result) {
             System.out.println("memberDto = " + memberDto);
+        }
+    }
+
+    //필드 직접 접근2 - 별칭을 사용하는 경우 - dto 와의 이름이 맞지 않을 때 별칭 as 사용하기
+    //서브쿼리에 별칭 사용하는 경우
+    @Test
+    public void findUserDtoByFields() {
+        List<UserDto> result = queryFactory
+                .select(Projections.fields(UserDto.class,
+                        member.username, member.age))
+                .from(member)
+                .fetch();
+
+        for (UserDto userDto : result) {
+            System.out.println("userDto1 = " + userDto);
+        }
+
+
+        //dto 와의 이름이 맞지 않을 때 별칭 as 사용하기
+        List<UserDto> result2 = queryFactory
+                .select(Projections.fields(UserDto.class,
+                        member.username.as("name"), member.age))
+                .from(member)
+                .fetch();
+
+        for (UserDto userDto : result2) {
+            System.out.println("userDto2 = " + userDto);
+        }
+
+
+        //서브쿼리에 별칭 사용하는 경우
+        //서브쿼리의 경우에는 ExpressionUtils.as() 로 감싸줘야 실행 가능하다.
+        QMember memberSub = new QMember("memberSub");
+        List<UserDto> result3 = queryFactory
+                .select(Projections.fields(UserDto.class,
+                        member.username.as("name"),
+
+                        ExpressionUtils.as(
+                                JPAExpressions
+                                        .select(memberSub.age.max())
+                                        .from(memberSub)
+                                , "age")))
+
+                .from(member)
+                .fetch();
+
+        for (UserDto userDto : result3) {
+            System.out.println("userDto3 = " + userDto);
+        }
+    }
+
+    //생성자 사용
+    @Test
+    public void findDtoByConstructor() {
+        List<MemberDto> result = queryFactory
+                .select(Projections.constructor(MemberDto.class, member.username, member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+
+
+        //필드 사용과 다르게, 생성자로 프로젝션 하는 경우에는 조회문의 이름 지정이 달라도
+        // 타입과 순서에 맞으면 알아서 데이터 들어간다. 별칭 안해줘도
+        List<UserDto> result2 = queryFactory
+                .select(Projections.constructor(UserDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+
+        for (UserDto userDto : result2) {
+            System.out.println("userDto = " + userDto);
         }
     }
 
