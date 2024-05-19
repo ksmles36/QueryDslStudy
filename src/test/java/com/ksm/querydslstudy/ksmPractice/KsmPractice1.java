8ca973ff51c5338a5893c3bd7103f2895587abd1
kimseongmin <ksmles36@gmail.com>
2024-05-19 22:30:46 +0900

    쿼리dsl 연습했음.

---
+++ src/test/java/com/ksm/querydslstudy/ksmPractice/KsmPractice1.java
@@ -0,0 +1,70 @@
+package com.ksm.querydslstudy.ksmPractice;
+
+import com.ksm.querydslstudy.dto.MemberDto;
+import com.ksm.querydslstudy.dto.QMemberDto;
+import com.ksm.querydslstudy.entity.Member;
+import com.ksm.querydslstudy.entity.Team;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static com.ksm.querydslstudy.entity.QMember.member;
+
+@SpringBootTest
+@Transactional
+public class KsmPractice1 {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+
+    @Test
+    public void test123() {
+        List<MemberDto> fetch = queryFactory
+                .select(new QMemberDto(member.username, member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : fetch) {
+            System.out.println("memberDto = " + memberDto);
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
